0fb02e6cd13a73cc6ceda1c1a076a2e90cc07bc8
Ricardo Branco <28.ricardobranco@gmail.com>
2014-01-25 05:55:12 +0000

    Change: AcademicInformation column_name description

--- db/migrate/20140113230058_create_academic_informations.rb
+++ db/migrate/20140113230058_create_academic_informations.rb
@@ -3,7 +3,7 @@ class CreateAcademicInformations < ActiveRecord::Migration
     create_table :academic_informations do |t|
       t.datetime :b_date
       t.datetime :e_date
-      t.text :escription
+      t.text :description
       t.float :grade
       t.references :academic_degree, index: true
       t.references :course, index: true
